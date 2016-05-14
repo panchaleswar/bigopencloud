9d17bc0c9dcc9b6ac41d4926418f02afaf0e4c0e
panchaleswar <nayak.p@gmail.com>
2016-05-14 00:05:33 -0700

    Delete install-java1.pp

--- puppet-scripts/install-java1.pp
+++
@@ -1,9 +0,0 @@
-class tmobilejava8 {
-  file { "/tmp/tmobilejava8": source => 'puppet:///modules/tmobilejava8/jdk-8u65-linux-x64.rpm
-', }
-  package {'bocjava8':
-    provider => 'rpm',
-    source => "/tmp/tmobilejava8.rpm",
-    require => File["/tmp/tmobilejava8.rpm"],
-  }
-}
